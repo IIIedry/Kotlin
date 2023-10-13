4c6eb89abc2545effa3a1bb7dd1fbfeed5ac1f5a
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-13 23:20:16 +0600

    add pathway 3.1 part 3.3.7

--- homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
+++ homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.homework2
 fun main() {
     var favoriteActor: String? = "Sandra Oh"
 
-    val lengthOfName = if (favoriteActor != null) {
-        favoriteActor.length
-    } else {
-        0
-    }
+    val lengthOfName = favoriteActor?.length ?: 0
 
     println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
