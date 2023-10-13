d095bee3f8d8ace798e04da99a8ff17b5e51d9e0
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-13 23:35:38 +0600

    add pathway 3.1 part 5.4.1

--- homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
+++ homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -4,8 +4,13 @@ fun main() {
     val trickFunction = trick
     trick()
     trickFunction()
+    treat()
 }
 
 val trick = {
     println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
 }
