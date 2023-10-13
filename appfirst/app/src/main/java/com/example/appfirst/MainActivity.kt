318c7febd2f832100eaa4171559d7a4345a9a91b
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-13 22:55:50 +0600

    exercise 1 pathway 3 unit 4.7 part

--- appfirst/app/src/main/java/com/example/appfirst/MainActivity.kt
+++ appfirst/app/src/main/java/com/example/appfirst/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,13 +29,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppFirstTheme {
+            AppFirstTheme{
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    GreetingImage( stringResource(R.string.happy_birthday_text), "From Emma")
                 }
             }
         }
@@ -87,9 +85,10 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = false)
 @Composable
-fun BirthdayCardPreview() {
+private fun BirthdayCardPreview() {
     AppFirstTheme {
-        GreetingImage("Happy Birthday Sam!", "From Emma")
+        GreetingImage( stringResource(R.string.happy_birthday_text),
+            stringResource(R.string.signature_text))
     }
 }
 
