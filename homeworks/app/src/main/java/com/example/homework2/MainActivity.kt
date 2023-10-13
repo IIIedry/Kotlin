1770d1a943e138b65cb133c9f4cc684662bdd59d
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-13 23:44:00 +0600

    add pathway 3.1 part 6.8.1

--- homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
+++ homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -2,31 +2,14 @@ package com.example.homework2
 
 
 fun main() {
-    val amanda = Person("Amanda", 33, "play tennis", null)
-    val atiqah = Person("Atiqah", 28, "climb", amanda)
+    val winningBid = Bid(5000, "Private Collector")
 
-    amanda.showProfile()
-    atiqah.showProfile()
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
 }
 
+class Bid(val amount: Int, val bidder: String)
 
-class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
-    fun showProfile() {
-        println("Name: $name")
-        println("Age: $age")
-        if(hobby != null) {
-            print("Likes to $hobby. ")
-        }
-        if(referrer != null) {
-            print("Has a referrer named ${referrer.name}")
-            if(referrer.hobby != null) {
-                print(", who likes to ${referrer.hobby}.")
-            } else {
-                print(".")
-            }
-        } else {
-            print("Doesn't have a referrer.")
-        }
-        print("\n\n")
-    }
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+    return bid?.amount ?: minimumPrice
 }
