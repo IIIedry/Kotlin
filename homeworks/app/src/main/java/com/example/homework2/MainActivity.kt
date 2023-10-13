5d3fbde07fec2df6fd5ab94740d519c817870596
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-13 23:18:08 +0600

    add pathway 3.1 part 3.3.1

--- homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
+++ homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.homework2
 
 fun main() {
-    var number: Int? = 10
-    println(number)
-
-    number = null
-    println(number)
+    var favoriteActor: String = "Sandra Oh"
+    println(favoriteActor.length)
 }
