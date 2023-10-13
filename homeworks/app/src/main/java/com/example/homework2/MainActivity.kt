dd1ee3a8332d979900fc9bb3bcd9a2f3371247d3
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-10-13 23:19:21 +0600

    add pathway 3.1 part 3.3.4

--- homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
+++ homeworks/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.homework2
 
 fun main() {
     var favoriteActor: String? = "Sandra Oh"
-    println(favoriteActor!!.length)
+
+    if (favoriteActor != null) {
+        println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+    }
 }
